c5d093422944d7195960ea525a3cdf37d026640c
franklin-silva41 <franklin.oliveira@bandtec.com.br>
2022-02-17 13:12:05 -0300

    implementação da especialidade

--- api-teste-sptech/src/main/resources/data.sql
+++ api-teste-sptech/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 insert into colaborador
-(colaborador_id, nome, nivelprofissional, semestreatual,donopost,especialidade.id_especialidade)
+(colaborador_id, nome, nivelprofissional, semestreatual,donopost) 
 values
-(null,'Franklin','estagiario','4','frank',1),
+(null,'Franklin','estagiario','4','frank'),
 (null,'Henrique','estagiario','3','frank'),
 (null,'Leticia','estagiario','2','frank'),
 (null,'Matheus','estagiario','2','frank'),
 (null,'Felipe','estagiario','2','Eduardo'),
 (null,'Eduardo','lider tecnico','*','Eduardo');
 
-insert into especialidade
+insert into Especialidades
 (id_especialidade, especialidade, nivel)
 values
-(null, 'backend','basico');
+(null,'backend','basico');
